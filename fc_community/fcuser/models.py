4792a813bc1c556513e41d8fbae9600eb55897bb
HyeongLee <leeceo97@naver.com>
2021-01-10 06:03:31 +0900

    회원가입 구현

---
+++ fc_community/fcuser/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Fcuser(models.Model):
+    #verbose_name:username,password등이 아니라 뒤에적은 이름이 출력됨(편리성)
+    username=models.CharField(max_length=64,verbose_name='사용자명')
+    password=models.CharField(max_length=64,verbose_name='비밀번호')
+    # auto_now_add:현재시간을 자동으로 추가해주는것 
+    registered_dttm=models.DateTimeField(auto_now_add=True,verbose_name='등록시간')
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table='fastcampus_fcuser'
+        #fcuser가 아닌 패스트캠퍼스 사용자로 출력하기 위한것
+        verbose_name="패스트캠퍼스 사용자"
+        #plural은 뒤에 복수형으로 나오는걸 방지하기위한것
+        verbose_name_plural='패스트캠퍼스 사용자'
