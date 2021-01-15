2a35db83609f343042a9c13512218b282456b8df
HyeongLee <leeceo97@naver.com>
2021-01-16 06:25:49 +0900

    홈화면,게시판,태그작성

--- fc_community/board/models.py
+++ fc_community/board/models.py
@@ -3,4 +3,22 @@ from django.db import models
 # Create your models here.
 
 class Board(models.Model):
-    pass
+    objects=models.Manager()
+    title=models.CharField(max_length=128,verbose_name='제목', null=True)
+    # 텍스트필드는 최대길이설정 필요x
+    contents=models.TextField(verbose_name='내용', null=True, default='')
+    # 사용자가 탈퇴하면 사용자가 쓴모든글은 삭제 CASCADE
+    writer=models.ForeignKey('fcuser.Fcuser',on_delete=models.CASCADE, verbose_name='작성자', null=True)
+    # auto_now_add:현재시간을 자동으로 추가해주는것 
+    registered_dttm=models.DateTimeField(auto_now_add=True,verbose_name='등록시간', null=True)
+    tags=models.ManyToManyField('tag.Tag',verbose_name='태그')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table='fastcampus_board'
+        #fcuser가 아닌 패스트캠퍼스 사용자로 출력하기 위한것
+        verbose_name="패스트캠퍼스 게시글"
+        #plural은 뒤에 복수형으로 나오는걸 방지하기위한것
+        verbose_name_plural='패스트캠퍼스 게시글'
--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -2,33 +2,30 @@ from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.contrib.auth.hashers import make_password, check_password
 from .models import Fcuser
+from .forms import LoginForm
 # Create your views here.
 
 def home(request):
-    user_id=request.session.get('user')
-    
-    if user_id:
-        fcuser=Fcuser.objects.get('user')
+    return render(request,'home.html')
+
+def logout(request):
+    if request.session.get('user'):
+        del(request.session['user'])
+
+    return redirect('/')
+
 
 def login(request):
-    if request.method=='GET':
-        return render(request,'login.html')
-    elif request.method=='POST':
-        username=request.POST.get('username',None)
-        password=request.POST.get('password',None)
-        res_data={}
-        if not(username and password):
-            res_data['error']='모든 값을 입력해야합니다.'
-        else:
-            fcuser=Fcuser.objects.get(username=username)
-            if check_password(password,Fcuser.password):
-                request.session['user']=fcuser.id
-                return redirect('/')
-                
-            else:
-                res_data['error']='비밀번호가 틀렸습니다.'
-        return render(request,'login.html',res_data)
+    if request.method=='POST':
+        form=LoginForm(request.POST)
+        #is_valid()는 로그인검증 함수
+        if form.is_valid():
+            request.session['user']=form.user_id
+            return redirect('/')
+    else:
+        form=LoginForm()
 
+    return render(request,'login.html',{'form':form})
 
 def register(request):
     if request.method=='GET':
